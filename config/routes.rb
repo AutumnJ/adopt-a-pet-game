87f75d53687ce30ead4d14ba0ac5482da6dac253
Autumn Kangas <autumnkangas@gmail.com>
2018-04-14 22:18:51 -0500

    set up routes front end will route to

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # resources :dogs
+  # resources :cats
+  resources :users, only: [:show] do
+    resources :cats, :dogs, only: [:index, :create, :show, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
