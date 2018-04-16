12147a6d522ca2f94f63c928fb1b9e123f6ec510
Autumn Kangas <autumnkangas@gmail.com>
2018-04-15 23:49:40 -0500

    stubbing out app to test actions/reducers with API

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import CatsPage from "./containers/CatsPage";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <div>
+                  <header className="App-header">
+                    <h1 className="App-title">Welcome to Adopt A Pet</h1>
+                  </header>
+                    <Route path='/cats' component={CatsPage} />
+                </div>
+              )}
+            />
+            <Route path='/cats' component={CatsPage} />
+          </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
+
+//MOVE INTO CONTAINERS or COMPONENTS?
+//REMOVE link to?
+//Fix weird link nesting
