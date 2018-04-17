d3e9f06a2090494256b1689c6d5dafb3e10b74b4
Autumn Kangas <autumnkangas@gmail.com>
2018-04-17 00:15:30 -0500

    validate zip code and display errors in view

---
+++ client/src/components/FormErrors.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const FormErrors = ({formErrors}) =>
+  <div className='formErrors'>
+    {Object.keys(formErrors).map((fieldName, i) => {
+      if(formErrors[fieldName].length > 0){
+        return (
+          <p key={i}><i>{formErrors[fieldName]}</i></p>
+        )        
+      } else {
+        return '';
+      }
+    })}
+  </div>
+
+  // add {fieldName} to <p> return if there are mltpl fields
