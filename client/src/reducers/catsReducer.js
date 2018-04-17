3344c060771e7d0c530bf075331716ae52003cc4
Autumn Kangas <autumnkangas@gmail.com>
2018-04-17 00:16:27 -0500

    comment out unused reducers for now

--- client/src/reducers/catsReducer.js
+++ client/src/reducers/catsReducer.js
@@ -1,10 +1,9 @@
 export default function catsReducer(state = { loading: false, cats: [] }, action) {
   switch (action.type) {
-    case 'LOADING_CATS':
-      return Object.assign({}, state, { loading: true })
-    case 'GET_CATS':
-      return Object.assign({}, state, { loading: false, cats: action.payload })
-      // return { loading: false, cats: action.payload }
+    // case 'LOADING_CATS':
+    //   return Object.assign({}, state, { loading: true })
+    // case 'GET_CATS':
+    //   return Object.assign({}, state, { loading: false, cats: action.payload })
     case 'CREATE_CAT':
       return {...state, cats: state.cats.concat(action.payload)}
     default:
