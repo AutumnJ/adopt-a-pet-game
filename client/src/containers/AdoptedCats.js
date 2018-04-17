9596bf6d7e0dcaddb30ae836b70ab173a6a9750a
Autumn Kangas <autumnkangas@gmail.com>
2018-04-17 15:19:08 -0500

    starting to stub out this container

--- client/src/containers/AdoptedCats.js
+++ client/src/containers/AdoptedCats.js
@@ -2,40 +2,71 @@ import React, { Component } from "react";
 // import uuid from 'uuid';
 import { connect } from "react-redux";
 // import { Route, Switch, Link } from "react-router-dom";
+import { Grid, Row, Col, Thumbnail } from 'react-bootstrap';
 
 class AdoptedCats extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      cats: [],
+      adopted: this.props.adopted
     }
   }
 
   render(){
-    let { cats, actions } = this.props;
+
+    // const thumbnailKitties = this.state.adopted.map(kitty =>
+    //   <Col xs={6} md={4}>
+    //     <Thumbnail src={kitty.photo} alt={kitty.name}>
+    //       <h3>{kitty.name}</h3>
+    //       <p>{kitty.description}</p>
+    //     </Thumbnail>
+    //   </Col>
+    // )
+    console.log(this.state.adopted[0])
 
     // console.log(this.props.cats.cats.length)
-    return (
-      <div>
-        <h1 className="cats-header">"You got me!<small className="text-muted">Now take me home!</small>"</h1>
 
-        <p>Add cats to adopted section</p>
-      </div>
-    );
+    //THIS NEEDS TO SCALE TO SHOW AS MANY CATS AS THERE ARE!
+    if (this.state.adopted[0]) {
+      return (
+        <div>
+          <h1 className="cats-header">"You got me!<small className="text-muted">Now take me home!</small>"</h1>
+          <Grid>
+            <Row>
+              <Col xs={6} md={4}>
+                <Thumbnail src={this.state.adopted[0].photo} alt={this.state.adopted[0].name}>
+                  <h3>{this.state.adopted[0].name}</h3>
+                  <p>{this.state.adopted[0].description}</p>
+                </Thumbnail>
+              </Col>
+            </Row>
+          </Grid>;
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1 className="cats-header">"You got me!<small className="text-muted">Now take me home!</small>"</h1>
+        </div>
+      )
+    }
   }
 }
 
 
-const mapStateToProps = state => {
-  return {
-    cats: state.cats
-  };
-};
-
-// const mapDispatchToProps = dispatch => {
-//   return { actions: bindActionCreators(actions, dispatch) };
+//NOT SURE ANY OF THIS IS NEEDED -> CAN THIS JUST BE A COMPONENT? 
+// const mapStateToProps = state => {
+//   return {
+//     cats: state.cats
+//   };
 // };
 
-export default connect(mapStateToProps)(AdoptedCats);
+// // const mapDispatchToProps = dispatch => {
+// //   return { actions: bindActionCreators(actions, dispatch) };
+// // };
+
+// export default connect(mapStateToProps)(AdoptedCats);
+
+export default AdoptedCats
