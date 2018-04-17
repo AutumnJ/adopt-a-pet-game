3e40ea347e8ca0d85e26ae588744f7604e665083
Autumn Kangas <autumnkangas@gmail.com>
2018-04-17 15:21:40 -0500

    updates during testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # resources :dogs
   # resources :cats
   resources :users, only: [:show] do
-    resources :cats, :dogs, only: [:index, :create, :show, :destroy]
+    resources :cats, :dogs, only: [:index, :create, :show]
   end
 
-  resources :cats, only: [:create]
+  resources :cats, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
