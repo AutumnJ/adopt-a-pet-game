4f3f0eacaedf3ca68428468c61aeff1b92854f84
Autumn Kangas <autumnkangas@gmail.com>
2018-04-17 00:17:41 -0500

    use bootstrap navbar styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+// import '../App.css'; STYLE NAVBAR?
 
 const NavBar = () => {
   return (
-    <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-      <NavLink 
-        style={{ marginRight: '10px' }} 
-        to="/"
-      >
-        Home
-      </NavLink>
-      <NavLink 
-        style={{ marginRight: '10px' }} 
-        to="/cats"
-      >
-        Cats
-      </NavLink>
+    <div style={{ borderBottom: '2px solid #C5CBE3', paddingBottom: '10px', marginBottom: '12px' }}>
+      <Navbar class="navbar navbar-custom">
+        <Navbar.Header>
+          <Navbar.Brand>
+             <NavLink to="/">
+              Home
+            </NavLink>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1}>
+            <NavLink to="/cats">
+              Cats
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
