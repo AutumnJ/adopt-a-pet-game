e9345a316d54aac86269b41182e72c054f25a22c
Autumn Kangas <autumnkangas@gmail.com>
2018-04-17 00:18:23 -0500

    FINALLY get external API and internal API fetch requests squared away, subbed those for fetch from interal data file

--- client/src/actions/catActions.js
+++ client/src/actions/catActions.js
@@ -1,5 +1,9 @@
 import fetch from 'isomorphic-fetch';
+import { staticCats } from '../data'
 
+//After organized, comment back in external API fetch, internal API data persistence and addCat action
+
+//DON'T NEED THIS CURRENTLY:
 // const getCats = (cats) => {
 //   return {
 //     type: "GET_CATS",
@@ -8,25 +12,11 @@ import fetch from 'isomorphic-fetch';
 // };
 
 const addCat = (cat) => {
-  //I think this is a promise resolution issue?
-  console.log("in add cat action")
-  console.log(cat)
   return {
-    //update reducer so cat is added to state when it is persisted to DB
     type: "CREATE_CAT",
-    payload: cat
+    payload: staticCats
   };
 };
-
-const postCats = (cats) => {
-  //define function to parse/map cats? or define in var
-  return fetch(//post each cat to internal API)
-    //after cats are posted to internal API, fetch cats from internal API
-    //then run getCats action
-  )
-}
-
-//Map cats to an array of objects, then send each object to create cat 
 
 const mapCats = (cats) => {
   let catList = cats.petfinder.pets.pet
@@ -52,74 +42,53 @@ const mapCats = (cats) => {
 }
 
 //Fix hard-coded userId
+//move URL into .env
 const createCat = (cat) => {
-  // return dispatch => {
-    return fetch(`http://localhost:3001/users/1/cats`, {
-      method: "POST",
-      // mode: 'no-cors',
-      headers: {
-        "Accept":"application/json",
-        "Content-Type":"application/json"
-      },
-      body: JSON.stringify(cat)
-    })
-    .then(response => response.json())
-      // .then(function (response) {
-      //   return response.json();
-      // })
-  // //     .then(response => console.log(response))
-  //     .then(cat => new Promise(function(resolve, reject) {
-  //       dispatch(addCat(cat))
+  return fetch(`http://localhost:3001/users/1/cats`, {
+    method: "POST",
+    headers: {
+      "Accept":"application/json",
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(cat)
+  })
+  .then(response => response.json())
+  .catch(error => console.log(error));
 
-  //       setTimeout(() => {
-  //         resolve(cat);
-  //       }, 5000);
-  //     }))
-      // .then(cat => dispatch(addCat(cat)));
-      // })
-      // .catch(error => console.log(error));
-  // };
 }
 
 //Fix hard-coded location
-//need to assign unique ID to user in view and pass it in as part of state 
+//Need to assign User_id at some point - in view? in mapping action? and set in state by setting it as action.payload.user_id?
+
+//Comment the below back in to start pulling from external API
+
+// export const fetchCats = () => {
+//   return async dispatch => {
+//     return fetch(`${process.env.REACT_APP_EXTERNAL_API_URL}&location=60625&format=json&animal=cat&count=5`)
+//       .then(response => response.json())
+//       .then(function(cats) {
+//         let mappedResponse = mapCats(cats);
+//         return mappedResponse
+//       })
+//       .then(async (mappedResponse) => {
+//         let kitties = await Promise.all(mappedResponse.map((cat) => {
+//           return createCat(cat);
+//         }))
+//         return kitties
+//       })
+//       .then(function(kitties) {
+//         kitties.forEach(cat => {
+//           dispatch(addCat(cat))
+//         })
+//       })
+//       .catch(error => console.log(error));
+//   };
+// };
+
+//from the below, it looks like I can dispatch all the cats at once, in the above fetch 
+//ONLY USING THIS TO DISPATCH USING MY DATA FILE:
 export const fetchCats = () => {
   return dispatch => {
-    return fetch(`${process.env.REACT_APP_EXTERNAL_API_URL}&location=60625&format=json&animal=cat&count=16`)
-      .then(response => response.json())
-      .then(function(cats) {
-        let mappedResponse = mapCats(cats);
-        return mappedResponse
-      })
-      // .then(cats => mapCats(cats))
-      // .then(cats => new Promise(function(resolve, reject) {
-      //   let catList = mapCats(cats);
-
-      //   setTimeout(() => {
-      //     resolve(cats);
-      //   }, 5000);
-      // }))
-
-      //THIS WORKS BUT IS FIRING BEFORE THE FETCH HAS FINISHED. 
-      .then(function(mappedResponse) {
-        let kitties = []
-        mappedResponse.forEach(cat => {
-          kitties.push(createCat(cat));
-        })
-        return kitties
-      })
-      .then(function(kitties) {
-        kitties.forEach(cat => {
-          dispatch(addCat(cat))
-        })
-      })
-      // .then(mappedResponse => mappedResponse.forEach(cat => { 
-      //   createCat(cat);
-      // }))
-      // .then(cats => mapCats(cats))
-      // .then(cats => dispatch(getCats(cats)))
-      .catch(error => console.log(error));
-  };
+      dispatch(addCat())
+    }
 };
-
-//need to find out how to keep key in .env
