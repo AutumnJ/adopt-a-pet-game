2e73357e10f0b62d370b06b221df5fe4ccd9867a
Autumn Kangas <autumnkangas@gmail.com>
2018-04-17 15:19:27 -0500

    finalizing the grid component and the photo changes therein

--- client/src/containers/CatsGrid.js
+++ client/src/containers/CatsGrid.js
@@ -6,7 +6,10 @@ import { bindActionCreators } from "redux";
 import * as actions from "../actions/catActions";
 import { Table } from 'react-bootstrap';
 import '../App.css';
-import CatGridList from '../components/CatGridList'
+import CatGridItem from '../components/CatGridItem'
+import IMG_5695 from '../IMG_5695.jpg'
+import Adopted from '../Adopted.png'
+import AdoptedCats from './AdoptedCats'
 
 class CatsGrid extends Component {
 
@@ -14,17 +17,72 @@ class CatsGrid extends Component {
       super();
 
       this.state = {
-        pets: []
+        cats: [],
+        adoptedCats: []
       }
     }
-    
-    //on click sub in '' for pic
-    //randomly disappear sub in ''
+
+    //Should this just go in constructor?
+    componentWillMount() {
+      this.setState({
+        cats: this.props.cats.cats,
+        adoptedCats: []
+      })
+    }
+
+    handleOnClick = (event) => {
+      event.preventDefault();
+      const { id } = event.target;
+      const { cats, adoptedCats } = this.state
+
+      const kitty = cats.find( cat => cat.id === parseInt(id) );
+      const kittyInd = cats.findIndex( cat => cat.id === parseInt(id));
+      if (kitty.photo !== "/static/media/IMG_5695.7d23606d.jpg" && kitty.photo !== "/static/media/Adopted.e366e9ec.png") {
+        let adoptedKitty = Object.assign({}, kitty);
+        adoptedCats.push(adoptedKitty);
+        kitty.photo = Adopted;
+
+        this.setState({
+          cats: cats,
+          adoptedCats: adoptedCats
+        })
+
+        this.disappearCat(cats);
+
+      } else {
+        this.disappearCat(cats);
+      }
+    }
+
+    disappearCat = (cats) => {
+      let replacement = this.replacementInd();
+
+      if (cats[replacement].photo !== "/static/media/IMG_5695.7d23606d.jpg" && cats[replacement].photo !== "/static/media/Adopted.e366e9ec.png") {
+        let goneKitty = cats[replacement];
+        goneKitty.photo = IMG_5695;
+
+        this.setState({
+          cats: cats,
+          // adoptedCats: adoptedCats
+        })
+      } else if (cats.find( cat => cat.photo !== "/static/media/IMG_5695.7d23606d.jpg" && cat.photo !== "/static/media/Adopted.e366e9ec.png")) {
+        this.disappearCat(cats);
+      } else {
+        return;
+      }
+    } 
+
+    replacementInd() {
+      return (Math.floor((Math.random() * 16) + 1) - 1);
+    }
 
     render(){
-    const { cats, actions, msg } = this.props;
-    
-    console.log(this.props)
+    const { cats, id } = this.props.cats;
+    // console.log("cats round 1:")
+    // console.log(cats)
+    // console.log("cat state round 1:" )
+    // console.log(this.state)
+
 
     return (
       <div>
@@ -32,31 +90,32 @@ class CatsGrid extends Component {
          <Table bordered condensed>
           <tbody id="table-container">
             <tr>
-              <CatGridList photo={cats.cats[0].photo} />
-              <CatGridList photo={cats.cats[1].photo} />
-              <CatGridList photo={cats.cats[2].photo} />
-              <CatGridList photo={cats.cats[3].photo} />
+              <CatGridItem photo={cats[0].photo} onClick={this.handleOnClick} id={cats[0].id} />
+              <CatGridItem photo={cats[1].photo} onClick={this.handleOnClick} id={cats[1].id} />
+              <CatGridItem photo={cats[2].photo} onClick={this.handleOnClick} id={cats[2].id} />
+              <CatGridItem photo={cats[3].photo} onClick={this.handleOnClick} id={cats[3].id} />
             </tr>
             <tr>
-              <CatGridList photo={cats.cats[4].photo} />
-              <CatGridList photo={cats.cats[5].photo} />
-              <CatGridList photo={cats.cats[6].photo} />
-              <CatGridList photo={cats.cats[7].photo} />
+              <CatGridItem photo={cats[4].photo} onClick={this.handleOnClick} id={cats[4].id}/>
+              <CatGridItem photo={cats[5].photo} onClick={this.handleOnClick} id={cats[5].id} />
+              <CatGridItem photo={cats[6].photo} onClick={this.handleOnClick} id={cats[6].id} />
+              <CatGridItem photo={cats[7].photo} onClick={this.handleOnClick} id={cats[7].id} />
             </tr>
             <tr>
-              <CatGridList photo={cats.cats[8].photo} />
-              <CatGridList photo={cats.cats[9].photo} />
-              <CatGridList photo={cats.cats[10].photo} />
-              <CatGridList photo={cats.cats[11].photo} />
+              <CatGridItem photo={cats[8].photo} onClick={this.handleOnClick} id={cats[8].id} />
+              <CatGridItem photo={cats[9].photo} onClick={this.handleOnClick} id={cats[9].id} />
+              <CatGridItem photo={cats[10].photo} onClick={this.handleOnClick} id={cats[10].id} />
+              <CatGridItem photo={cats[11].photo} onClick={this.handleOnClick} id={cats[11].id} />
             </tr>
             <tr>
-              <CatGridList photo={cats.cats[12].photo} />
-              <CatGridList photo={cats.cats[13].photo} />
-              <CatGridList photo={cats.cats[14].photo} />
-              <CatGridList photo={cats.cats[15].photo} />
+              <CatGridItem photo={cats[12].photo} onClick={this.handleOnClick} id={cats[12].id} />
+              <CatGridItem photo={cats[13].photo} onClick={this.handleOnClick} id={cats[13].id} />
+              <CatGridItem photo={cats[14].photo} onClick={this.handleOnClick} id={cats[14].id} />
+              <CatGridItem photo={cats[15].photo} onClick={this.handleOnClick} id={cats[15].id} />
             </tr>
           </tbody>
         </Table>
+        <AdoptedCats adopted={this.state.adoptedCats}/>
       </div>
     );
   }
