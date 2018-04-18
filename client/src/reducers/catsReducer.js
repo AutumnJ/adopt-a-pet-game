3741360ffe28fa3ba03d6648a10cd49cb72e1a20
Autumn Kangas <autumnkangas@gmail.com>
2018-04-18 03:40:14 -0500

    testing reducer to prevent unwanted changes to state

--- client/src/reducers/catsReducer.js
+++ client/src/reducers/catsReducer.js
@@ -1,11 +1,13 @@
-export default function catsReducer(state: cats = [], action) {
+export default function catsReducer(state = {cats:[]}, action) {
   switch (action.type) {
     // case 'LOADING_CATS':
     //   return Object.assign({}, state, { loading: true })
     // case 'GET_CATS':
     //   return Object.assign({}, state, { loading: false, cats: action.payload })
     case 'CREATE_CAT':
-      return {...state, cats: state.concat(action.payload)}
+      return Object.assign ({}, { cats: action.payload })
+    case 'CLEAR_STATE':
+      return { cats:[] }
     default:
       return state;
   }
