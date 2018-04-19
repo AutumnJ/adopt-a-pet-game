e3f8dd48b1cfb21fea53e0eeb4d0b5854c5b0f41
Autumn Kangas <autumnkangas@gmail.com>
2018-04-19 17:24:18 -0500

    fix styling for photos

--- client/src/components/CatGridItem.js
+++ client/src/components/CatGridItem.js
@@ -4,10 +4,10 @@ import '../App.css';
  
 const CatGridItem = ({ photo, onClick, id }) => {
   //cound do <Image src={photo} responsive rounded alt="KITTIES"/>
-  const renderPet = <img src={ photo } id={ id } style={{'height' : '75%', 'width' : '85%', 'objectFit' : 'contain'}} onClick={ onClick } alt="KITTIES"/>
+  const renderPet = <img src={ photo } id={ id } style={{'height' : '100%', 'maxWidth' : '100%', 'objectFit' : 'contain'}} onClick={ onClick } alt="KITTIES"/>
  
   return (
-    <td style={{'width' : '25%', 'height' : '25%'}}>
+    <td style={{'width' : '200px', 'height' : '200px'}}>
       {renderPet}
     </td>
   );
