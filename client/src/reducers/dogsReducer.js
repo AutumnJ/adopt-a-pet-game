0d7cce3e53741d4cfda9562efc9bd9e377512a62
Autumn Kangas <autumnkangas@gmail.com>
2018-04-19 01:08:16 -0500

    action to add dogs after rails api returns JSON

--- client/src/reducers/dogsReducer.js
+++ client/src/reducers/dogsReducer.js
@@ -1,6 +1,17 @@
-export default function dogsReducer(state = [], action) {
+export default function dogsReducer(state = {dogs:[]}, action) {
   switch (action.type) {
+    // case 'LOADING_CATS':
+    //   return Object.assign({}, state, { loading: true })
+    // case 'GET_CATS':
+    //   return Object.assign({}, state, { loading: false, cats: action.payload })
+    case 'CREATE_DOG':
+      return Object.assign ({}, { dogs: action.payload })
+    case 'CLEAR_STATE':
+      return { dogs:[] }
     default:
       return state;
   }
 }
+
+//delete dogs -> fire action from which page?
+//
