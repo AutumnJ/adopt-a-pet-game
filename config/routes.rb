28f44fdc9085eca31c5cb274ffeb3673646ee85b
Autumn Kangas <autumnkangas@gmail.com>
2018-04-19 01:07:52 -0500

    update to remove nested routes as they no longer make sense

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # resources :dogs
   # resources :cats
-  resources :users, only: [:show] do
-    resources :cats, :dogs, only: [:index, :create, :show]
-  end
+  # resources :users, only: [:show] do
+  #   resources :cats, :dogs, only: [:index, :create, :show]
+  # end
 
-  resources :cats, only: [:destroy]
+  resources :cats, only: [:index, :create, :show, :destroy]
+  
+  resources :dogs, only: [:index, :create, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
