3151043de13aeee9dfd2f13c8ef399d8daf6e4e3
Autumn Kangas <autumnkangas@gmail.com>
2018-04-19 01:10:01 -0500

    add actions to facilitate external API call and push to internal API

--- client/src/actions/dogActions.js
+++ client/src/actions/dogActions.js
@@ -1,1 +1,67 @@
 import fetch from 'isomorphic-fetch';
+import StockPuppy from '../lib/StockPuppy.png'
+
+const addDog = (dogs) => {
+  return {
+    type: "CREATE_DOG",
+    payload: dogs
+  };
+};
+
+const mapDogs = (dogs) => {
+  let dogList = dogs.petfinder.pets.pet
+
+  const mappedList = dogList.map(dog => {
+    let dogObj = {};
+    let photo = dog.media.photos ? dog.media.photos.photo[3].$t : StockPuppy;
+
+    dogObj.name = dog.name.$t;
+    dogObj.age = dog.age.$t;
+    dogObj.size = dog.size.$t;
+    dogObj.sex = dog.sex.$t;
+    dogObj.breed = dog.breeds.breed.$t;
+    dogObj.contact_phone = dog.contact.phone.$t;
+    dogObj.contact_email = dog.contact.email.$t;
+    dogObj.contact_city = dog.contact.city.$t;
+    dogObj.photo = photo;
+    // dogObj.user_id = userId;
+    dogObj.description = dog.description.$t;
+
+    return dogObj;
+  })
+  return mappedList;
+}
+
+const createDog = (dog) => {
+  return fetch(`http://localhost:3001/dogs`, {
+    method: "POST",
+    headers: {
+      "Accept":"application/json",
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(dog)
+  })
+  .then(response => response.json())
+  .catch(error => console.log(error));
+}
+
+export const fetchDogs = (zip) => {
+  return async dispatch => {
+    return fetch(`${process.env.REACT_APP_EXTERNAL_API_URL}&location=${zip}&format=json&animal=dog&count=16`)
+      .then(response => response.json())
+      .then(function(dogs) {
+        let mappedResponse = mapDogs(dogs);
+        return mappedResponse
+      })
+      .then(async (mappedResponse) => {
+        let doggies = await Promise.all(mappedResponse.map((dog) => {
+          return createDog(dog);
+        }))
+        return doggies
+      })
+      .then(function(doggies) {
+          dispatch(addDog(doggies))
+        })
+      .catch(error => console.log(error));
+  };
+};
