67a9c2dc3db01f88ec0a28b49e24f2e7f371aacf
Autumn Kangas <autumnkangas@gmail.com>
2018-04-19 17:25:18 -0500

    add function to return a random pet

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,5 +1,9 @@
 class Cat < ApplicationRecord
 
   validates :name, presence: :true
+
+  def self.random
+    Cat.limit(1).order("RANDOM()")
+  end
   
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,9 @@
 class Dog < ApplicationRecord
 
   validates :name, presence: :true
+
+  def self.random
+    Dog.limit(1).order("RANDOM()")
+  end
   
 end
