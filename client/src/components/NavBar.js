638d46a5ed6bec2b8f07e45616af70270b9f3b83
Autumn Kangas <autumnkangas@gmail.com>
2018-04-20 16:25:56 -0500

    redid navbar to remove error

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,45 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
-// import '../App.css'; STYLE NAVBAR?
 
-const NavBar = () => {
-  return (
-    <div style={{ borderBottom: '2px solid #C5CBE3', paddingBottom: '10px', marginBottom: '12px' }}>
-      <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand id="nav-header">
-             <NavLink to="/">
-              Home
-            </NavLink>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-          <NavItem eventKey={1}>
-            <NavLink to="/cats">
-              Cats
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <Nav>
-          <NavItem eventKey={1}>
-            <NavLink to="/dogs">
-              Dogs
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <Nav>
-          <NavItem eventKey={1}>
-            <NavLink to="/randompet">
-              Random Pet
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
+const NavBar = props => (
+  <nav className="navbar navbar-default">
+    <div className="container-fluid">
+      <ul className="nav navbar-nav">
+        <li>
+          <NavLink to="/" id="home">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/cats">Cats</NavLink>
+        </li>
+        <li>
+          <NavLink to="/dogs">Dogs</NavLink>
+        </li>
+        <li>
+          <NavLink to="/randompet">Random Pet</NavLink>
+        </li>
+      </ul>
     </div>
-  );
-}
+  </nav>
+);
 
 export default NavBar;
+
+
 
         
