14d873ae8a0e35ba5c7d816a09e60540b999bcf9
Autumn Kangas <autumnkangas@gmail.com>
2018-04-20 16:25:15 -0500

    cleaned up code, fixed margin

--- client/src/components/CatGridItem.js
+++ client/src/components/CatGridItem.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import '../App.css';
-// import { Image } from 'react-bootstrap';
  
 const CatGridItem = ({ photo, onClick, id }) => {
-  //cound do <Image src={photo} responsive rounded alt="KITTIES"/>
+
   const renderPet = <img src={ photo } id={ id } style={{'height' : '100%', 'maxWidth' : '100%', 'objectFit' : 'contain'}} onClick={ onClick } alt="KITTIES"/>
  
   return (
