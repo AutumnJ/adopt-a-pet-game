5280f4072814c0ca105f38ce634a061ac35a6d22
Autumn Kangas <autumnkangas@gmail.com>
2018-04-20 16:26:08 -0500

    cleaned up code, fixed margin

--- client/src/components/PetErrorPage.js
+++ client/src/components/PetErrorPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Image } from 'react-bootstrap';
+
 import IMG_5695LG from '../lib/IMG_5695LG.jpg'
 import BigPup from '../lib/BigPup.jpg'
 
@@ -15,11 +16,9 @@ export const PetErrorPage = ({ pet }) => {
         <h2><small className="text-muted">Oops! Maybe all the { title } got homes.</small></h2>
         <h4>Navigate to the <Link to={'/'}>homepage</Link> to adopt again!</h4>
       </div>
-      <div style={{"paddingTop" : "10px"}}>
+      <div style={{"paddingTop" : "15px"}}>
         <Image src={photo} alt="sad kitty" rounded/> 
       </div>
     </div>
   )
 }
-
-// <Image src="https://autumnj.github.io//assets/images/IMG_5193.JPG" alt="sad kitty" rounded/> 
