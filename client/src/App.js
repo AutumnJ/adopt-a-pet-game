b08e8ad18af2f6cbd8a930da364e0c6f1444add9
Autumn Kangas <autumnkangas@gmail.com>
2018-04-20 16:24:45 -0500

    played around with footer, cleaned up code

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import { Link } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
 import CatsPage from "./containers/CatsPage";
@@ -9,7 +8,6 @@ import DogsPage from "./containers/DogsPage";
 import QuestionPage from "./containers/QuestionPage";
 import RandomPetPage from "./containers/RandomPetPage";
 
-//Get rid of NavBar?
 class App extends Component {
   render() {
     return (
