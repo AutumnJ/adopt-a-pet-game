4c9236eaeacdd194d00cf5b5bda9a0c5ab0929b3
Autumn Kangas <autumnkangas@gmail.com>
2018-04-21 15:53:42 -0500

    add reducers for game play and refresh for dogs

--- client/src/reducers/dogsReducer.js
+++ client/src/reducers/dogsReducer.js
@@ -1,9 +1,20 @@
-export default function dogsReducer(state = {dogs:[], randomDog: [], adoptedDogs: []}, action) {
+export default function dogsReducer(state = {dogs:[], randomDog: [], adoptedDogs: [], dogGame:[]}, action) {
   switch (action.type) {
     case 'CREATE_DOG':
       return Object.assign ({}, { ...state, dogs: action.payload })
     case 'ADD_DOG':
       return Object.assign ({}, { ...state, randomDog: action.payload })
+    case 'ADOPT_DOG':
+      return {...state, adoptedDogs: state.adoptedDogs.concat(action.payload)} 
+    case 'CLEAR_GAME':
+      return Object.assign({}, {...state, adoptedDogs: [], dogGame: []})
+    case 'PLAY_GAME':
+      let newArray = []
+
+      state.dogs.forEach(dog => {
+        newArray.push(Object.assign({}, dog))
+      })
+      return Object.assign ({}, { ...state, dogGame: newArray})
     default:
       return state;
   }
