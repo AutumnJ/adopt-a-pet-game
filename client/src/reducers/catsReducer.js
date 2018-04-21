df5313eda7607d45d26885fb0b8c370770bb1f36
Autumn Kangas <autumnkangas@gmail.com>
2018-04-21 11:46:40 -0500

    add reducers to facilitate game

--- client/src/reducers/catsReducer.js
+++ client/src/reducers/catsReducer.js
@@ -1,11 +1,22 @@
-export default function catsReducer(state = {cats:[], randomCat:[]}, action) {
+export default function catsReducer(state = {cats:[], randomCat:[], adoptedCats: [], catGame:[]}, action) {
   switch (action.type) {
     case 'CREATE_CAT':
       //want to replace previous state, not concat onto it
-      return Object.assign ({}, { ...state, cats: action.payload })
+      return Object.assign ({}, { ...state, cats: action.payload})
     case 'ADD_CAT':
       //want to replace previous state, not concat onto it
       return Object.assign ({}, { ...state, randomCat: action.payload })
+    case 'ADOPT_CAT':
+      return {...state, adoptedCats: state.adoptedCats.concat(action.payload)} 
+    case 'CLEAR_GAME':
+      return Object.assign({}, {...state, adoptedCats: [], catGame: []})
+    case 'PLAY_GAME':
+      let newArray = []
+
+      state.cats.forEach(cat => {
+        newArray.push(Object.assign({}, cat))
+      })
+      return Object.assign ({}, { ...state, catGame: newArray})
     default:
       return state;
   }
