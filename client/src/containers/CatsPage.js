61e7fccd543939bd1550e2a38d7e1ffd7a6904ad
Autumn Kangas <autumnkangas@gmail.com>
2018-04-22 21:06:42 -0500

    map dispatch to props and pass to child

--- client/src/containers/CatsPage.js
+++ client/src/containers/CatsPage.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 import CatsGrid from './CatsGrid'
 import { PetErrorPage } from '../components/PetErrorPage'
+import { adoptCat, clearGame, playGame, updatePhoto } from '../actions/catActions'
 
 class CatsPage extends Component {
 
@@ -20,7 +22,7 @@ class CatsPage extends Component {
             <h1> /\___/\  KITTIES!  /\___/\</h1>
             <h2><small className="text-muted">Grab them before they disappear!</small></h2>
           </div>
-          <CatsGrid />
+          <CatsGrid adoptedCats={this.props.adoptedCats} catGame={this.props.catGame} actions={this.props.actions} />
         </div>
       );
     }
@@ -29,9 +31,15 @@ class CatsPage extends Component {
 
 const mapStateToProps = state => {
   return {
-    cats: state.cats.cats
+    adoptedCats: state.cats.adoptedCats,
+    cats: state.cats.cats,
+    catGame: state.cats.catGame
   };
 };
 
-export default connect(mapStateToProps, null)(CatsPage);
+const mapDispatchToProps = dispatch => {
+  return { actions: bindActionCreators( { adoptCat, clearGame, playGame, updatePhoto }, dispatch) };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CatsPage);
 
