9255a92715a7f561cd5bb33d78d41928e7699c50
Autumn Kangas <autumnkangas@gmail.com>
2018-04-22 22:31:53 -0500

    set this up to pass props to two child components - grid and adopted pets

--- client/src/containers/DogsPage.js
+++ client/src/containers/DogsPage.js
@@ -2,9 +2,12 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
-import DogsGrid from './DogsGrid'
 import { PetErrorPage } from '../components/PetErrorPage'
+import PetsGrid from '../components/PetsGrid'
 import { adoptDog, clearGame, playGame, updatePhoto } from '../actions/dogActions'
+import LittlePup from '../lib/LittlePup.jpg'
+import Adopted from '../lib/Adopted.png'
+import AdoptedPets from '../components/AdoptedPets'
 
 class DogsPage extends Component {
 
@@ -18,11 +21,22 @@ class DogsPage extends Component {
 
       return (
         <div>
-          <div className="cats-header">
+          <div className="dogs-header">
             <h1> DOGGIES! </h1>
             <h2><small className="text-muted">Grab them before they disappear!</small></h2>
           </div>
-          <DogsGrid adoptedDogs={this.props.adoptedDogs} dogGame={this.props.dogGame} actions={this.props.actions} />
+          <PetsGrid 
+            gamePets={this.props.dogGame} 
+            adopt={this.props.actions.adoptDog} 
+            updatePhoto={this.props.actions.updatePhoto}
+            clearGame={this.props.actions.clearGame}
+            playGame={this.props.actions.playGame}
+            adoptedImg={Adopted} 
+            adoptedImgStatic={"/static/media/Adopted.e366e9ec.png"} 
+            takenImg={LittlePup} 
+            takenImgStatic={"/static/media/LittlePup.df8ef6dd.jpg"} 
+            />
+          <AdoptedPets adopted={this.props.adoptedDogs}/>
         </div>
       );
     }
