c0c7885e65f4732b5dac92488b9bbd37d8b57128
Autumn Kangas <autumnkangas@gmail.com>
2018-04-22 21:07:06 -0500

    map dispatch to props and pass to child

--- client/src/containers/DogsPage.js
+++ client/src/containers/DogsPage.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 import DogsGrid from './DogsGrid'
 import { PetErrorPage } from '../components/PetErrorPage'
+import { adoptDog, clearGame, playGame, updatePhoto } from '../actions/dogActions'
 
 class DogsPage extends Component {
 
@@ -20,7 +22,7 @@ class DogsPage extends Component {
             <h1> DOGGIES! </h1>
             <h2><small className="text-muted">Grab them before they disappear!</small></h2>
           </div>
-          <DogsGrid />
+          <DogsGrid adoptedDogs={this.props.adoptedDogs} dogGame={this.props.dogGame} actions={this.props.actions} />
         </div>
       );
     }
@@ -29,9 +31,15 @@ class DogsPage extends Component {
 
 const mapStateToProps = state => {
   return {
-    dogs: state.dogs.dogs
+    dogs: state.dogs.dogs,
+    adoptedDogs: state.dogs.adoptedDogs,
+    dogGame: state.dogs.dogGame
   };
 };
 
-export default connect(mapStateToProps, null)(DogsPage);
+const mapDispatchToProps = dispatch => {
+  return { actions: bindActionCreators( { adoptDog, clearGame, playGame, updatePhoto }, dispatch) };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DogsPage);
 
