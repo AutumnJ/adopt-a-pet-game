df048783fe6df7da08bc09bbd8e55def0581d23f
Autumn Kangas <autumnkangas@gmail.com>
2018-05-15 09:10:57 -0500

    remove code from project review, no need for upvote functionality at this time

--- client/src/components/PetGridItem.js
+++ client/src/components/PetGridItem.js
@@ -1,49 +1,8 @@
-
-// import { Button, ButtonToolbar } from 'react-bootstrap';
 import React, { Component } from "react";
-// import fetch from 'isomorphic-fetch';
  
 class PetGridItem extends Component {
-
-  // constructor() {
-  //   super();
-
-  //   this.state = {
-  //     upvotes: 0
-  //   };
-  // }
-
-  // Upvote = () => {
-  //   this.setState({
-  //     upvotes: (this.state.upvotes + 1)
-  //   })
-  // }
-
-  // callApi = () => {
-  //   console.log('a')
-  //   fetch(`http://localhost:3001/cafdsfdsfdsts`)
-  //   .then(response => {
-
-  //     if (response.ok) {
-
-  //       console.log('b')
-
-  //       return response.json()
-  //       } else {
-  //       throw new Error(response.statusText)
-  //     }
-  //     })
-      
-  //   .then(data => console.log('c', data))
-  //   .catch(err => console.log('d', err));
-  //     console.log('e')
-      
-  //     // a e b c + data  
-  //     // a e d 
-  // }
  
   render() {
-      // console.log(this.props)
       const renderPet = <img src={ this.props.photo } id={ this.props.id } style={{'height' : '100%', 'maxWidth' : '100%', 'objectFit' : 'contain'}} onClick={ this.props.onClick } alt="KITTIES"/>
     return (
       <td style={{'width' : '200px', 'height' : '200px'}}>
@@ -55,14 +14,3 @@ class PetGridItem extends Component {
 };
  
 export default PetGridItem;
-
-//from render:
-          // <ButtonToolbar>
-          //   <Button bsStyle="primary" bsSize="small" onClick={this.Upvote}>
-          //     Like
-          //   </Button>
-          //   <Button bsStyle="primary" bsSize="small" onClick={this.callApi}>
-          //     Call Api
-          //   </Button>
-          // </ButtonToolbar>
-          // {this.state.upvotes}
