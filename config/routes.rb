31e079e823501dc5c4d14a3c97f8b0bea3572fad
Autumn Kangas <autumnkangas@gmail.com>
2018-05-15 09:10:35 -0500

    clean up unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   
   resources :dogs, only: [:create]
 
-    get'/dogs/random', to: 'dogs#random'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get'/dogs/random', to: 'dogs#random'
+
 end
